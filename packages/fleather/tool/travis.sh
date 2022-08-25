166a99e8b38d95a372cc971a4a457bc7820dfe6e
Alan Mantoux <alan.mantoux@gmail.com>
2022-08-25 08:44:52 +0200

    Remove Travis files

--- packages/fleather/tool/travis.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-set -e
-
-flutter test || $TRAVIS_BUILD_DIR/flutter/bin/flutter test
-
-dart format -o none --set-exit-if-changed lib/
-dart format -o none --set-exit-if-changed test/
--- tool/travis.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-set -e
-
-cd "packages/$1"
-./tool/travis.sh
