676e6f4f42f1fe28adfe44351d609794b8ce4df4
Alan Mantoux <alan.mantoux@gmail.com>
2022-10-28 10:51:13 +0200

    Support for Markdown decoding

--- packages/parchment/lib/src/convert/markdown.dart
+++ packages/parchment/lib/src/convert/markdown.dart
@@ -12,17 +12,279 @@ class ParchmentMarkdownCodec extends Codec<Delta, String> {
   const ParchmentMarkdownCodec();
 
   @override
-  Converter<String, Delta> get decoder =>
-      throw UnimplementedError('Decoding is not implemented yet.');
+  Converter<String, Delta> get decoder => _ParchmentMarkdownDecoder();
 
   @override
   Converter<Delta, String> get encoder => _ParchmentMarkdownEncoder();
 }
 
+class _ParchmentMarkdownDecoder extends Converter<String, Delta> {
+  final List<Map<String, dynamic>?> _attributesByStyleLength = [
+    null,
+    {'i': true}, // _
+    {'b': true}, // **
+    {'i': true, 'b': true} // _**
+  ];
+  final RegExp _headingRegExp = RegExp(r'(#+) *(.+)');
+  final _styleRegExp = RegExp(
+    r'(([*_])(\*{2}|_{2})(?<italic_bold_text>.*?[^ \3\2])\3\2)|((\*{2}|_{2})([*_])(?<bold_italic_text>.*?[^ \7\6])\7\6)|(((\*{1,2})|(_{1,2}))(?<bold_or_italic_text>.*?[^ \10])\10)',
+  );
+
+  // TODO: implement full solution as per:
+  // https://www.michaelperrin.fr/blog/2019/02/advanced-regular-expressions
+  final RegExp _linkRegExp =
+      RegExp(r'\[(?<text>.+)\]\((?<url>[^ ]+)(?: "(?<title>.+)")?\)');
+  final RegExp _ulRegExp = RegExp(r'^( *)\* +(.*)');
+  final RegExp _olRegExp = RegExp(r'^( *)\d+[.)] +(.*)');
+  final RegExp _bqRegExp = RegExp(r'^> *(.*)');
+  final RegExp _codeRegExpTag = RegExp(r'^( *)```');
+  final RegExp _inlineCodeRegExp = RegExp(r'`(.+?)`');
+  bool _inBlockStack = false;
+
+//  final List<String> _blockStack = [];
+//  int _olDepth = 0;
+
+  @override
+  Delta convert(String input) {
+    final lines = input.split('\n');
+    final delta = Delta();
+
+    for (var line in lines) {
+      _handleLine(line, delta);
+    }
+
+    return delta;
+  }
+
+  void _handleLine(String line, Delta delta,
+      [Map<String, dynamic>? attributes]) {
+    if (_handleBlockQuote(line, delta, attributes)) {
+      return;
+    }
+    if (_handleBlock(line, delta, attributes)) {
+      return;
+    }
+    if (_handleHeading(line, delta, attributes)) {
+      return;
+    }
+
+    if (line.isNotEmpty) {
+      _handleSpan(line, delta, true, attributes);
+    }
+  }
+
+  /// Markdown supports headings and blocks within blocks (except for within code)
+  /// but not blocks within headers, or ul within
+  bool _handleBlock(String line, Delta delta,
+      [Map<String, dynamic>? attributes]) {
+    final match = _codeRegExpTag.matchAsPrefix(line);
+    if (match != null) {
+      _inBlockStack = !_inBlockStack;
+      return true;
+    }
+    if (_inBlockStack) {
+      delta.insert(
+          '$line\n',
+          ParchmentAttribute.code
+              .toJson()); // TODO: replace with?: {'quote': true})
+      // Don't bother testing for code blocks within block stacks
+      return true;
+    }
+
+    if (_handleOrderedList(line, delta, attributes) ||
+        _handleUnorderedList(line, delta, attributes)) {
+      return true;
+    }
+
+    return false;
+  }
+
+  // all blocks are supported within bq
+  bool _handleBlockQuote(String line, Delta delta,
+      [Map<String, dynamic>? attributes]) {
+    var match = _bqRegExp.matchAsPrefix(line);
+    var span = match?.group(1);
+    if (span != null) {
+      final newAttributes = <String, dynamic>{
+        'block': 'quote'
+      }; // ParchmentAttribute.bq.toJson();
+      if (attributes != null) {
+        newAttributes.addAll(attributes);
+      }
+      // all blocks are supported within bq
+      _handleLine(span, delta, newAttributes);
+      return true;
+    }
+    return false;
+  }
+
+  /// ol is supported within ol and bq, but not supported within ul
+  bool _handleOrderedList(String line, Delta delta,
+      [Map<String, dynamic>? attributes]) {
+    var match = _olRegExp.matchAsPrefix(line);
+    var span = match?.group(2);
+    if (span != null) {
+// TODO: support nesting
+//      var depth =  match.group(1).length / 3;
+      Map<String, dynamic> newAttributes = ParchmentAttribute.ol.toJson();
+      if (attributes != null) {
+        newAttributes.addAll(attributes);
+      }
+      // There's probably no reason why you would have other block types on the same line
+      _handleSpan(span, delta, true, newAttributes);
+      return true;
+    }
+    return false;
+  }
+
+  bool _handleUnorderedList(String line, Delta delta,
+      [Map<String, dynamic>? attributes]) {
+    var match = _ulRegExp.matchAsPrefix(line);
+    var span = match?.group(2);
+    if (span != null) {
+//      var depth = match.group(1).length / 3;
+      Map<String, dynamic> newAttributes = ParchmentAttribute.ul.toJson();
+      if (attributes != null) {
+        newAttributes.addAll(attributes);
+      }
+      // There's probably no reason why you would have other block types on the same line
+      _handleSpan(span, delta, true, newAttributes);
+      return true;
+    }
+    return false;
+  }
+
+  bool _handleHeading(String line, Delta delta,
+      [Map<String, dynamic>? attributes]) {
+    final match = _headingRegExp.matchAsPrefix(line);
+    final levelTag = match?.group(1);
+    if (levelTag != null) {
+      final level = levelTag.length;
+      final Map<String, dynamic> newAttributes = {
+        'heading': level
+      }; // ParchmentAttribute.heading.withValue(level).toJson();
+      if (attributes != null) {
+        newAttributes.addAll(attributes);
+      }
+
+      final span = match?.group(2);
+      if (span == null) {
+        return false;
+      }
+      _handleSpan(
+          span,
+          delta,
+          true,
+          attributes != null
+              ? (attributes..addAll(newAttributes))
+              : newAttributes);
+      return true;
+    }
+
+    return false;
+  }
+
+  void _handleSpan(String span, Delta delta, bool addNewLine,
+      Map<String, dynamic>? outerStyle) {
+    var start = _handleStyles(span, delta, outerStyle);
+    span = span.substring(start);
+
+    if (span.isNotEmpty) {
+      start = _handleLinks(span, delta, outerStyle);
+      span = span.substring(start);
+    }
+
+    if (span.isNotEmpty) {
+      if (addNewLine) {
+        delta.insert('$span\n', outerStyle);
+      } else {
+        delta.insert(span, outerStyle);
+      }
+    } else if (addNewLine) {
+      delta.insert('\n', outerStyle);
+    }
+  }
+
+  int _handleStyles(
+      String span, Delta delta, Map<String, dynamic>? outerStyle) {
+    var start = 0;
+
+    final matches = _styleRegExp.allMatches(span);
+    for (final match in matches) {
+      if (match.start > start) {
+        if (span.substring(match.start - 1, match.start) == '[') {
+          delta.insert(span.substring(start, match.start - 1), outerStyle);
+          start = match.start -
+              1 +
+              _handleLinks(span.substring(match.start - 1), delta, outerStyle);
+          continue;
+        } else {
+          delta.insert(span.substring(start, match.start), outerStyle);
+        }
+      }
+      final String? text;
+      final int styleLength;
+      if (match.namedGroup('italic_bold_text') != null) {
+        text = match.namedGroup('italic_bold_text');
+        styleLength = 3;
+      } else if (match.namedGroup('bold_italic_text') != null) {
+        text = match.namedGroup('bold_italic_text');
+        styleLength = 3;
+      } else {
+        text = match.namedGroup('bold_or_italic_text');
+        styleLength = match.group(10)?.length ?? 0;
+      }
+
+      if (text == null) {
+        return start;
+      }
+
+      final newStyle =
+          Map<String, dynamic>.from(_attributesByStyleLength[styleLength]!);
+      if (outerStyle != null) {
+        newStyle.addAll(outerStyle);
+      }
+      _handleSpan(text, delta, false, newStyle);
+      start = match.end;
+    }
+
+    return start;
+  }
+
+  int _handleLinks(String span, Delta delta, Map<String, dynamic>? outerStyle) {
+    var start = 0;
+
+    final matches = _linkRegExp.allMatches(span);
+    for (final match in matches) {
+      if (match.start > start) {
+        delta.insert(span.substring(start, match.start)); //, outerStyle);
+      }
+
+      final text = match.group(1);
+      final href = match.group(2);
+      if (text == null || href == null) {
+        return start;
+      }
+      final Map<String, dynamic> newAttributes = {
+        'a': href
+      }; // ParchmentAttribute.link.fromString(href).toJson();
+      if (outerStyle != null) {
+        newAttributes.addAll(outerStyle);
+      }
+      _handleSpan(text, delta, false, newAttributes);
+      start = match.end;
+    }
+
+    return start;
+  }
+}
+
 class _ParchmentMarkdownEncoder extends Converter<Delta, String> {
-  static const kBold = '**';
-  static const kItalic = '_';
-  static final kSimpleBlocks = <ParchmentAttribute, String>{
+  // TODO: as per https://daringfireball.net/projects/markdown/basics
+  //  emphasis = "*" || "_"
+  //  strong emphasis = "**" || "__"
+  //  combined = "***" || "___"
+  static final simpleBlocks = <ParchmentAttribute, String>{
     ParchmentAttribute.bq: '> ',
     ParchmentAttribute.ul: '* ',
     ParchmentAttribute.ol: '1. ',
@@ -133,10 +395,11 @@ class _ParchmentMarkdownEncoder extends Converter<Delta, String> {
     return ' ' * (text.length - result.length);
   }
 
+  //
   ParchmentStyle _writeInline(StringBuffer buffer, String text,
       ParchmentStyle style, ParchmentStyle currentStyle) {
     // First close any current styles if needed
-    for (var value in currentStyle.values) {
+    for (var value in currentStyle.values.reverse()) {
       if (value.scope == ParchmentAttributeScope.line) continue;
       if (style.containsSame(value)) continue;
       final padding = _trimRight(buffer);
@@ -178,11 +441,11 @@ class _ParchmentMarkdownEncoder extends Converter<Delta, String> {
   }
 
   void _writeBoldTag(StringBuffer buffer) {
-    buffer.write(kBold);
+    buffer.write('**');
   }
 
   void _writeItalicTag(StringBuffer buffer) {
-    buffer.write(kItalic);
+    buffer.write('_');
   }
 
   void _writeLinkTag(StringBuffer buffer, ParchmentAttribute<String> link,
@@ -210,8 +473,18 @@ class _ParchmentMarkdownEncoder extends Converter<Delta, String> {
     } else {
       if (close) return; // no close tag needed for simple blocks.
 
-      final tag = kSimpleBlocks[block];
+      final tag = simpleBlocks[block];
       buffer.write(tag);
     }
+  }
+}
+
+extension<E> on Iterable<E> {
+  Iterable<E> reverse() {
+    final reverse = <E>[];
+    for (final e in this) {
+      reverse.insert(0, e);
+    }
+    return reverse;
   }
 }
