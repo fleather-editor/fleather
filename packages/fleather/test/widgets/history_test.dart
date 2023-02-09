5355c55a392964a988a70d499425df66a3326471
Alan Mantoux <alan.mantoux@gmail.com>
2023-02-09 10:25:44 +0100

    Add tests to `FleatherController`

--- packages/fleather/test/widgets/history_test.dart
+++ packages/fleather/test/widgets/history_test.dart
@@ -33,79 +33,6 @@ void main() {
       stack = HistoryStack(Delta()..insert('Hello\n'));
     });
 
-    group('empty stack', () {
-      test('undo returns null', () {
-        expect(stack.canUndo, false);
-        expect(stack.undo(), isNull);
-      });
-
-      test('redo returns null', () {
-        expect(stack.canRedo, false);
-        expect(stack.redo(), isNull);
-      });
-    });
-
-    test('undoes twice', () {
-      final change = Delta()..insert('Hello world\n');
-      final otherChange = Delta()..insert('Hello world ok\n');
-      final exp = Delta()
-        ..retain(5)
-        ..delete(6);
-      final expOther = Delta()
-        ..retain(11)
-        ..delete(3);
-      stack.push(change);
-      stack.push(otherChange);
-      expect(stack.canUndo, true);
-      var act = stack.undo();
-      expect(act, expOther);
-      expect(stack.canUndo, true);
-      act = stack.undo();
-      expect(act, exp);
-    });
-
-    test('undoes too many times', () {
-      final change = Delta()..insert('Hello world\n');
-      stack.push(change);
-      var act = stack.undo();
-      expect(act, isNotNull);
-      act = stack.undo();
-      expect(act, isNull);
-      expect(stack.canUndo, false);
-    });
-
-    test('redoes twice', () {
-      final change = Delta()..insert('Hello world\n');
-      final otherChange = Delta()..insert('Hello world ok\n');
-      final exp = Delta()
-        ..retain(5)
-        ..insert(' world');
-      final expOther = Delta()
-        ..retain(11)
-        ..insert(' ok');
-      stack.push(change);
-      stack.push(otherChange);
-      stack.undo();
-      stack.undo();
-      expect(stack.canRedo, true);
-      var act = stack.redo();
-      expect(act, exp);
-      expect(stack.canRedo, true);
-      act = stack.redo();
-      expect(act, expOther);
-    });
-
-    test('redoes too many times', () {
-      final change = Delta()..insert('Hello world\n');
-      stack.push(change);
-      stack.undo();
-      var act = stack.redo();
-      expect(act, isNotNull);
-      act = stack.redo();
-      expect(act, isNull);
-      expect(stack.canRedo, false);
-    });
-
     test('pushes new items while index is at start', () {
       final change = Delta()..insert('Hello world\n');
       stack.push(change);
@@ -121,12 +48,6 @@ void main() {
       stack.push(change);
       var act = stack.undo();
       expect(act, Delta()..retain(5, {'b': null}));
-    });
-
-    test('Delta', () {
-      final base = Delta()..insert('Hello\n');
-      final inc = Delta()..retain(5, {'b': true});
-      expect(inc.invert(base), Delta()..retain(5, {'b': null}));
     });
   });
 
