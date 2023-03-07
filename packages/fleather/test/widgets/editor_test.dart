5e27dda20623fe9df51f0ac354600732ae9d1fb7
amantoux <amantoux@murex.com>
2023-03-07 15:53:30 +0100

    Linting

--- packages/fleather/test/widgets/editor_test.dart
+++ packages/fleather/test/widgets/editor_test.dart
@@ -1,7 +1,6 @@
 import 'package:fleather/fleather.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
