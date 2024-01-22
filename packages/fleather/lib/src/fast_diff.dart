dfc3fae593b54dd42b5f79b8cc5a44c05e8153ab
Amir Panahandeh <amirpanahandeh@yahoo.com>
2024-01-22 10:44:58 +0330

    Remove copyrights from dart files

--- packages/fleather/lib/src/fast_diff.dart
+++ packages/fleather/lib/src/fast_diff.dart
@@ -1,6 +1,3 @@
-// Copyright (c) 2018, the Zefyr project authors.  Please see the AUTHORS file
-// for details. All rights reserved. Use of this source code is governed by a
-// BSD-style license that can be found in the LICENSE file.
 import 'dart:math' as math;
 
 /// Performs a fast diff operation on two input strings based on provided
