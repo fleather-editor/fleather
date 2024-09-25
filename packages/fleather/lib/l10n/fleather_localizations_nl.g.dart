96c2588389af4185fe4bd8388ad9fb74ff96c371
Sjonnie2nd <r.loonstra@kpnmail.nl>
2024-09-25 18:25:25 +0200

    Add Dutch and German

---
+++ packages/fleather/lib/l10n/fleather_localizations_nl.g.dart
@@ -0,0 +1,48 @@
+import 'fleather_localizations.g.dart';
+
+/// The translations for Dutch (`nl`).
+class FleatherLocalizationsNl extends FleatherLocalizations {
+  FleatherLocalizationsNl([String locale = 'nl']) : super(locale);
+
+  @override
+  String get foregroundColorAutomatic => 'Automatisch';
+
+  @override
+  String get backgroundColorNoColor => 'Geen kleur';
+
+  @override
+  String get headingNormal => 'Standaard';
+
+  @override
+  String get headingLevel1 => 'Kop 1';
+
+  @override
+  String get headingLevel2 => 'Kop 2';
+
+  @override
+  String get headingLevel3 => 'Kop 3';
+
+  @override
+  String get headingLevel4 => 'Kop 4';
+
+  @override
+  String get headingLevel5 => 'Kop 5';
+
+  @override
+  String get headingLevel6 => 'Kop 6';
+
+  @override
+  String get addLinkDialogPasteLink => 'Een link invoegen';
+
+  @override
+  String get addLinkDialogApply => 'Toepassen';
+
+  @override
+  String get linkDialogOpen => 'Openen';
+
+  @override
+  String get linkDialogCopy => 'Kopiëren';
+
+  @override
+  String get linkDialogRemove => 'Verwijderen';
+}
