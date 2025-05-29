a041726c0cf15b1dc11216e7ff742b022be420d4
gellert234-dev <159481939+gellert234-dev@users.noreply.github.com>
2025-05-29 17:21:56 +0200

    fleather_localizations_hu.g.dart file upload

---
+++ packages/fleather/lib/l10n/fleather_localizations_hu.g.dart
@@ -0,0 +1,52 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'fleather_localizations.g.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Hungarian (`hu`).
+class FleatherLocalizationsEn extends FleatherLocalizations {
+  FleatherLocalizationsEn([String locale = 'hu']) : super(locale);
+
+  @override
+  String get foregroundColorAutomatic => 'Automatikus';
+
+  @override
+  String get backgroundColorNoColor => 'Nincs szín';
+
+  @override
+  String get headingNormal => 'Normál';
+
+  @override
+  String get headingLevel1 => 'Cím 1';
+
+  @override
+  String get headingLevel2 => 'Cím 2';
+
+  @override
+  String get headingLevel3 => 'Cím 3';
+
+  @override
+  String get headingLevel4 => 'Cím 4';
+
+  @override
+  String get headingLevel5 => 'Cím 5';
+
+  @override
+  String get headingLevel6 => 'Cím 6';
+
+  @override
+  String get addLinkDialogPasteLink => 'Link beillesztése';
+
+  @override
+  String get addLinkDialogApply => 'Alkalmaz';
+
+  @override
+  String get linkDialogOpen => 'Megnyítás';
+
+  @override
+  String get linkDialogCopy => 'Másolás';
+
+  @override
+  String get linkDialogRemove => 'Törlés';
+}
